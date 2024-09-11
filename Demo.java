26901863b56cfdcb583a9287f2d416fca4ac9a5a
Priti Karmakar <pritikarmakar834@gmail.com>
2024-09-11 13:08:23 +0530

    m4 is added

--- Demo.java
+++ Demo.java
@@ -1,5 +1,13 @@
-public class Test{
+public class Demo{
     public static void m2(){
+        String str="Hello from Demo";
+        System.out.println(str);
+    }
+    public static void m3(){
+        String str="Hello from Demo";
+        System.out.println(str);
+    }
+    public static void m4(){
         String str="Hello from Demo";
         System.out.println(str);
     }
